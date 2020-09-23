bdca85d8f2e744a259ed0d44a1208787db0d463e
Nouhaila <nouhailamokhtari4@gmail.com>
2020-09-23 01:07:40 +0100

    Updates

---
+++ precache-manifest.1766833f8b63edb5e68cfcca28a564a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002db022f6698cfc194733700e1f79ca",
+    "url": "/Recette-Reactjs/index.html"
+  },
+  {
+    "revision": "0327e20ba259180d2f2b",
+    "url": "/Recette-Reactjs/static/css/main.5ab2d6e7.chunk.css"
+  },
+  {
+    "revision": "097165f2d3182b25674d",
+    "url": "/Recette-Reactjs/static/js/2.fe3c042f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recette-Reactjs/static/js/2.fe3c042f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0327e20ba259180d2f2b",
+    "url": "/Recette-Reactjs/static/js/main.9323449e.chunk.js"
+  },
+  {
+    "revision": "897cbe66e1f4d4b7cee0",
+    "url": "/Recette-Reactjs/static/js/runtime-main.8ddb4d3e.js"
+  },
+  {
+    "revision": "aa89c7e094f4f3db2e9e9e24ed76d85e",
+    "url": "/Recette-Reactjs/static/media/background.aa89c7e0.jpg"
+  }
+]);
